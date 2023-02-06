bf7237e04026cf8aad570b3a49252ab91f427578
AlexZhadaev <pvt.vatnik@gmail.com>
2023-02-06 23:19:13 +0300

    Creating MainScreen storyboard and view controller, initial setup
    (cherry picked from commit 3b834028e6cfa60af70fbb90ab4a0224e0481246)

---
+++ MainScreen/MainScreenVC.swift
@@ -0,0 +1,51 @@
+//
+//  MainScreenVC.swift
+//  Millionaire
+//
+//  Created by Aleksei Zhadaev on 06.02.2023.
+//
+
+import UIKit
+
+
+final class MainScreenVC: UIViewController {
+  private enum SegueId {
+    static let gameRules = "toGameRules"
+    static let startGame = "toStartGame"
+  }
+  
+  @IBOutlet weak var logoImageView: UIImageView!
+  @IBOutlet weak var welcomeLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+  
+  @IBAction func rulesButtonPressed(_ sender: UIButton) {
+    performSegue(withIdentifier: SegueId.gameRules, sender: self)
+  }
+  
+  @IBAction func startButtonPressed(_ sender: UIButton) {
+    performSegue(withIdentifier: SegueId.startGame, sender: self)
+  }
+  
+  private func setupUI() {
+    logoImageView.layer.cornerRadius = logoImageView.frame.height / 2
+  }
+}
+
+//MARK: - Segue setup
+
+extension MainScreenVC {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      if segue.identifier == SegueId.gameRules {
+        // let destinationVC = segue.destination as ...
+      } else if segue.identifier == SegueId.startGame {
+        // let destinationVC = segue.destination as ...
+      }
+    }
+  
+}
