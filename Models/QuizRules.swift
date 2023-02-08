5d08bca1aacbbbfbda7a05332a09a82ce737c092
Saniya <janbolatovnas@gmail.com>
2023-02-08 20:09:51 +0600

    Did GameRulesVC

---
+++ Models/QuizRules.swift
@@ -0,0 +1,30 @@
+//
+//  QuizRules.swift
+//  Millionaire
+//
+//  Created by Dauletkhanova Saniya on 08.02.2023.
+//
+
+import UIKit
+
+struct QuizRules {
+    
+    let text = """
+Who Wants to Be a Millionaire? is a quiz competition where contestants have to correctly answer a series of multiple-choice questions in order to advance to the next level / question. There are 15 questions in total and each question is worth a specific amount of money and no time limit is placed on contestants to come up with an answer. Contestants also get three Lifelines to assist them if they get stuck on a particular question.
+
+The Who Wants to Be a Millionaire questions are structured according to five different Levels with each level increasing in difficulty. Each level contains three questions.
+
+Questions that are grouped into the same level will all be of similar difficulty. For example: Questions 1-3 make up the first Level and will contain the easiest questions. The second Level (Questions 4 – 6) will be slightly more difficult, followed by the third Level (Questions 7 – 9). The fourth Level (Questions 10-12) will consist of really difficult questions, followed by the fifth, and last, level (Questions 13 – 15) that will pose the most difficult questions of the game.
+
+Lifelines
+
+Contestants are allowed three Lifelines that they can use at any point during the game. Each Lifeline can only be used once.
+
+50/50 – removes two wrong answers from the multiple-choice selection, leaving the contestant with only one correct and one incorrect option. This means they have a 50/50 chance.
+
+Ask the Audience – the audience is asked the same question as the contestant and a quick poll is done to show their answers. If the chart shows a clear majority for a specific answer, this Lifeline can be extremely helpful, but it’s still up to the contestant whether or not to go with the results obtained from the audience.
+
+Phone a Friend – Contestants are allowed to make a 30-second call to a friend or family member and ask them if they know the answer to the question.
+
+"""
+}
--- UI/GameRules/GameRulesVC.swift
+++ UI/GameRules/GameRulesVC.swift
@@ -5,4 +5,25 @@
 //  Created by dmitry shcherba on 07.02.2023.
 //
 
+import UIKit
 import Foundation
+
+
+class GameRulesVC: UIViewController {
+    
+    @IBOutlet weak var rulesText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let quizRules = QuizRules()
+        let text = quizRules.text
+        rulesText.text = text
+        rulesText.font = UIFont.systemFont(ofSize: 15)
+        rulesText.sizeToFit()
+        rulesText.isEditable = false
+        rulesText.isScrollEnabled = true
+        
+        
+    }
+}
